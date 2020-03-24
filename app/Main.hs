f5719642ed29ed9971a293db4b5e82ae25401c54
Taskkill <johny95wil@gmail.com>
2020-03-24 18:18:28 +0100

    just small modification

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,8 @@ import Interpreter (evaluate)
 main :: IO ()
 main = do
   i <- getContents
-  let ast = parse $ scan i
+  let tokens = scan i
+  let ast = parse tokens
   r <- evaluate ast
   case r of
     Left msg -> print msg
