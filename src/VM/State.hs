3b489b1c2039543c93a2c2beef66f99867ca0ecb
Taskkill <johny95wil@gmail.com>
2020-05-06 14:51:44 +0200

    fix stack stuff and add some comments for future refactoring

--- src/VM/State.hs
+++ src/VM/State.hs
@@ -4,6 +4,7 @@ import qualified Data.Sequence as Seq
 import qualified Data.Map.Strict as Map
 
 import VM.Program.Value (Value(..), Index)
+import VM.Value (Pointer)
 import qualified VM.Value as Runtime
 import VM.Lib.Stack
 import VM.Frame (Frame(..))
@@ -17,7 +18,7 @@ type GlobalVarMap = Map.Map String Runtime.Value
 
 type SubprogramDir = Map.Map String InstructionAddress
 
-type OperandStack = Stack Runtime.Value
+type OperandStack = Stack Pointer
 
 type Output = Seq.Seq String
 
@@ -25,14 +26,16 @@ type ConstPool = Map.Map Index Value
 
 type FunctionList = [Value] -- only Function
 
+-- TODO: consider finally dividing Immutable and Program related stuff to different Structure
+-- State should be only VM realted State stuff
 data State
-  = CS { c      :: Context
-       , g      :: GlobalVarMap
-       , p      :: SubprogramDir
-       , s      :: OperandStack
-       , o      :: Output
-       , cp     :: ConstPool
-       , ia     :: InstructionAddress
-       , m      :: Memory
-       , gs     :: Globals
-       , fns    :: FunctionList }
+  = CS { c      :: Context -- VM State
+       , g      :: GlobalVarMap -- Program
+       , p      :: SubprogramDir -- Program
+       , s      :: OperandStack -- VM State
+       , o      :: Output -- VM State
+       , cp     :: ConstPool -- Program
+       , ia     :: InstructionAddress -- VM Program
+       , m      :: Memory -- VM Program
+       , gs     :: Globals -- 
+       , fns    :: FunctionList } -- Program
