87d293366fbd20192b9b76c08c6a7a3a98a95741
Taskkill <johny95wil@gmail.com>
2020-05-06 14:45:25 +0200

    refactor VM.Value into more correct structure

--- src/VM/Value.hs
+++ src/VM/Value.hs
@@ -4,17 +4,25 @@ import qualified VM.Program.Value as Program
 
 type Address = Int
 
--- 
--- TODO: CHANGE THIS
--- INSTEAD OF OPERAND - VMOBJECT
--- IT CAN BE INT, NULL, POINTER
--- DELETE RUNTIMEVALUE RINT, RNULL
--- RENAME RUNTIMEVALUE TO SOMETHING DESCRIPTIVE - LIKE OBJECT OR SOMETHING
--- LET'S SAY: DONE
+-- use GADT
+-- Pointer is distinct type which only goes on the Stack
+-- Value --> Memory
+-- All Values go to the memory
+-- Array, Object contain Pointers
+
 data Value
   = Int Int
   | Null
-  | Pointer Address
-  | Array { vals :: [Value] } -- really Runtime.Value ? I think so
-  | Object { vars :: [Value], super :: Value, cl :: Program.Value } -- Value here is Class  -- vars -> really Runtime.Value ?
-  | AntiValue -- just in case I decide to use it -> to initialize locals, globals and stuff
+  | Array { vals :: [Pointer] }
+  | Object { vars :: [Pointer], super :: Pointer, cl :: Program.Value }
+  | AntiValue
+
+data Pointer = Pointer Address
+
+-- data Value
+--   = Int Int
+--   | Null
+--   | Pointer Address
+--   | Array { vals :: [Value] } -- really Runtime.Value ? I think so
+--   | Object { vars :: [Value], super :: Value, cl :: Program.Value } -- Value here is Class  -- vars -> really Runtime.Value ?
+--   -- | AntiValue -- just in case I decide to use it -> to initialize locals, globals and stuff
