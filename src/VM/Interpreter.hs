fafc707e00e94d76f97afd592bf13ce5bb175bf7
Taskkill <johny95wil@gmail.com>
2020-05-07 15:25:33 +0200

    change names of State members

--- src/VM/Interpreter.hs
+++ src/VM/Interpreter.hs
@@ -23,7 +23,7 @@ import qualified VM.Lib.Stack as Stack
 runProgram :: Program -> State -> Output
 runProgram program state = output
   where
-    CS { o = output } =
+    CS { out = output } =
       runProgram' program state
       -- (CS
       --   { c = []
@@ -39,7 +39,7 @@ runProgram program state = output
 
 -- TODO: please refactor more -- this is ugly
 runProgram' :: Program -> State -> State
-runProgram' program@P { el = (_, addr), right = r } s@CS { ia = ia } =
+runProgram' program@P { el = (_, addr), right = r } s@CS { instaddr = ia } =
   case r of
     [] -> if ia > addr then s else runProgram' program cs
     _ -> runProgram' program cs
@@ -142,17 +142,17 @@ getNull mem = mem ! 0 -- NOTE: NULL is at index 0
 --
 
 runInstruction :: Instruction -> State -> State
-runInstruction (Lit index) cs@(CS { s = s, cp = cp, ia = ia, m = (si, mem) }) =
+runInstruction (Lit index) cs@(CS { stack = s, constpool = cp, instaddr = ia, memory = (si, mem) }) =
   -- get index's value from the const-pool (Int or Null) and push it to the Stack 
-  cs { s = s', ia = ia + 1, m = (si + 1, m') }
+  cs { stack = s', instaddr = ia + 1, memory = (si + 1, m') }
     where
       object = cp ! index
       m' = Map.insert si (valueToOperand_P object) mem
       s' = Stack.push (Pointer si) s
 
-runInstruction Array cs@(CS { s = s, ia = ia, m = (si, mem) }) =
+runInstruction Array cs@(CS { stack = s, instaddr = ia, memory = (si, mem) }) =
   -- pop initvalue, length from the Stack, create Array object and push it to the Stack
-  cs { s = Stack.push ptr s', ia = ia + 1, m = m' }
+  cs { stack = Stack.push ptr s', instaddr = ia + 1, memory = m' }
     where
       initVal = Stack.top s
       s'' = Stack.pop s
@@ -162,56 +162,56 @@ runInstruction Array cs@(CS { s = s, ia = ia, m = (si, mem) }) =
       m' = (si + 1, Map.insert si arr mem)
       ptr = Runtime.Pointer si
 
-runInstruction (Print format count) cs@(CS { s = s, o = o, ia = ia, m = (_, mem) }) =
+runInstruction (Print format count) cs@(CS { stack = s, out = o, instaddr = ia, memory = (_, mem) }) =
   -- pop count arguments from the Stack, modify the Output and push Null to the Stack
-  cs { s = s', o = o', ia = ia + 1 }
+  cs { stack = s', out = o', instaddr = ia + 1 }
     where
       (ptrs, s'') = Stack.popN count s
       vals = map (\(Pointer addr) -> mem ! addr) ptrs
       s' = Stack.push (Pointer 0) s'' -- address 0 is address of NULL -- Stack.push Runtime.Null s''
       o' = o |> (formatOut format vals)
 
-runInstruction (SetLocal index) cs@(CS { c = c, s = s, ia = ia, m = (_, mem) }) =
+runInstruction (SetLocal index) cs@(CS { ctx = c, stack = s, instaddr = ia, memory = (_, mem) }) =
   -- set index'th value in current frame to the top Stack's value
-  cs { c = c', s = s', ia = ia + 1 }
+  cs { ctx = c', stack = s', instaddr = ia + 1 }
     where
       ptr = Stack.top s
       s' = Stack.pop s
       c' = updateLocal index ptr c
 
-runInstruction (GetLocal index) cs@(CS { c = c, s = s, ia = ia }) =
+runInstruction (GetLocal index) cs@(CS { ctx = c, stack = s, instaddr = ia }) =
   -- push the index'th value from local frame to the top of the Stack
-  cs { s = s', ia = ia + 1 }
+  cs { stack = s', instaddr = ia + 1 }
     where
       ptr = getFromLocal index c
       s' = Stack.push ptr s
 
-runInstruction (SetGlobal index) cs@(CS { g = g, s = s, cp = cp, ia = ia }) =
+runInstruction (SetGlobal index) cs@(CS { gvm = g, stack = s, constpool = cp, instaddr = ia }) =
   -- set the global variable named as index'th String slot to the top of the Stack
-  cs { g = g', s = s', ia = ia + 1 }
+  cs { gvm = g', stack = s', instaddr = ia + 1 }
     where
       name = slot2String (cp ! index) cp
       ptr = Stack.top s
       s' = Stack.pop s
       g' = updateGlobal name ptr g
 
-runInstruction (GetGlobal index) cs@(CS { g = g, s = s, cp = cp, ia = ia }) =
+runInstruction (GetGlobal index) cs@(CS { gvm = g, stack = s, constpool = cp, instaddr = ia }) =
   -- get global variable named as index'th String slot and push it's value to the top of the Stack
-  cs { s = s', ia = ia + 1 }
+  cs { stack = s', instaddr = ia + 1 }
     where
       name = slot2String (cp ! index) cp
       ptr = getFromGlobal name g
       s' = Stack.push ptr s
 
-runInstruction Drop cs@(CS { s = s, ia = ia }) =
+runInstruction Drop cs@(CS { stack = s, instaddr = ia }) =
   -- pop single value
-  cs { s = s', ia = ia + 1 }
+  cs { stack = s', instaddr = ia + 1 }
     where
       s' = Stack.pop s
 
-runInstruction (Object index) cs@(CS { c = c, p = p, s = s, o = o, cp = cp, ia = ia, m = (si, mem) }) =
+runInstruction (Object index) cs@(CS { ctx = c, stack = s, constpool = cp, instaddr = ia, memory = (si, mem) }) =
   -- get Object's class at index, then check how many variables it has, pop so many values from the Stack and pop one more - superclass, then push the Object on the Stack
-  cs { s = s', ia = ia + 1, m = m' }
+  cs { stack = s', instaddr = ia + 1, memory = m' }
     where
       cl = cp ! index
       count = variableCount cl
@@ -222,9 +222,9 @@ runInstruction (Object index) cs@(CS { c = c, p = p, s = s, o = o, cp = cp, ia =
       m' = (si + 1, Map.insert si o mem)
       s' = Stack.push (Pointer si) s
 
-runInstruction (GetSlot index) cs@(CS { s = s, cp = cp, ia = ia, m = (_, mem) }) =
+runInstruction (GetSlot index) cs@(CS { stack = s, constpool = cp, instaddr = ia, memory = (_, mem) }) =
   -- pop Object from the Stack, index'th slot is String - Object variable name which's value will be pushed to the Stack
-  cs { s = s', ia = ia + 1 }
+  cs { stack = s', instaddr = ia + 1 }
     where
       Pointer addr = Stack.top s
       ob = mem ! addr
@@ -233,9 +233,9 @@ runInstruction (GetSlot index) cs@(CS { s = s, cp = cp, ia = ia, m = (_, mem) })
       ptr = getObjVar name ob
       s' = Stack.push ptr s''
 
-runInstruction (SetSlot index) cs@(CS { c = c, s = s, cp = cp, ia = ia, m = (si, mem) }) =
+runInstruction (SetSlot index) cs@(CS { ctx = c, stack = s, constpool = cp, instaddr = ia, memory = (si, mem) }) =
   -- pop value, pop Object, then change the variable named as index'th in the Object to the firstly poped value
-  cs { s = s', ia = ia + 1, m = m' }
+  cs { stack = s', instaddr = ia + 1, memory = m' }
     where
       Pointer valAddr = Stack.top s
       val = mem ! valAddr
@@ -252,7 +252,7 @@ runInstruction (SetSlot index) cs@(CS { c = c, s = s, cp = cp, ia = ia, m = (si,
 
 -- TODO: implement
 -- abstraction for the syntax sugars like: arr[0], arr[n], 3 + 4
-runInstruction (CallSlot index count) cs@(CS { c = c, p = p, s = s, o = o, cp = cp, ia = ia }) = undefined
+runInstruction (CallSlot index count) cs@(CS { ctx = c, labels = l, stack = s, out = o, constpool = cp, instaddr = ia }) = undefined
 -- this has to implement stuff like arr[i] and numA + numB
   -- when assigning variables to the Frame - check if the value is Object or Array - then store the name to the Object or Array 
   -- pop count values from the Stack, pop receiver Object, index points to the String - name of the method, then call the method
@@ -261,9 +261,9 @@ runInstruction (Label index) cs = cs
   -- names the current instruction, index points to the String with the name
   -- probably not gonna do anything
 
-runInstruction (Branch index) cs@(CS { p = p, s = s, cp = cp, ia = ia, m = (_, mem) }) =
+runInstruction (Branch index) cs@(CS { labels = l, stack = s, constpool = cp, instaddr = ia, memory = (_, mem) }) =
   -- pop value from the Stack, if the value is not Null, then jump to the index'th Label
-  cs { s = s', ia = ia' }
+  cs { stack = s', instaddr = ia' }
     where
       Pointer addr = Stack.top s
       val = mem ! addr
@@ -272,23 +272,23 @@ runInstruction (Branch index) cs@(CS { p = p, s = s, cp = cp, ia = ia, m = (_, m
       ia' =
         case val of
           Runtime.Null -> ia + 1
-          _ -> p ! (slot2String label cp) -- TODO: maybe check if label is (Slot addr)
+          _ -> l ! (slot2String label cp) -- TODO: maybe check if label is (Slot addr)
 
-runInstruction (Goto index) cs@(CS { p = p, cp = cp, ia = ia, m = (_, mem) }) =
+runInstruction (Goto index) cs@(CS { labels = l, constpool = cp, instaddr = ia, memory = (_, mem) }) =
   -- jump to the index'th Label
-  cs { ia = ia' }
+  cs { instaddr = ia' }
     where
       String str = cp ! index
       -- slot = mem ! str
       -- name = slot2String slot cp
-      ia' = p ! str
+      ia' = l ! str
 
-runInstruction (Return) cs@(CS { c = (Frame { caller = r } : fs), ia = ia }) =
-  cs { c = fs, ia = r }
+runInstruction (Return) cs@(CS { ctx = (Frame { caller = r } : fs), instaddr = ia }) =
+  cs { ctx = fs, instaddr = r }
 
-runInstruction (Call index count) cs@(CS { c = c, g = g, s = s, cp = cp, ia = ia, m = (_, mem), fns = fns }) =
+runInstruction (Call index count) cs@(CS { ctx = c, gvm = g, stack = s, constpool = cp, instaddr = ia, memory = (_, mem), fns = fns }) =
   -- pop count values from the Stack, the name of the function is at the indexth slot
-  state { ia = ia + 1 }
+  state { instaddr = ia + 1 }
     where
       name = slot2String (cp ! index) cp
       (ptrs, s') = Stack.popN count s
@@ -296,4 +296,4 @@ runInstruction (Call index count) cs@(CS { c = c, g = g, s = s, cp = cp, ia = ia
       initVals = replicate varC (Pointer 0) -- Runtime.Null
       bodyProg = instructions2Program b
       state =
-        runProgram' bodyProg $ cs { c = Frame { arguments = ptrs, variables = initVals, caller = ia } : c, s = s', ia = 0 }
+        runProgram' bodyProg $ cs { ctx = Frame { arguments = ptrs, variables = initVals, caller = ia } : c, stack = s', instaddr = 0 }
--- src/VM/State.hs
+++ src/VM/State.hs
@@ -29,13 +29,13 @@ type FunctionList = [Value] -- only Function
 -- TODO: consider finally dividing Immutable and Program related stuff to different Structure
 -- State should be only VM realted State stuff
 data State = CS
-  { c      :: Context -- VM State
-  , g      :: GlobalVarMap -- Program
-  , p      :: SubprogramDir -- Program
-  , s      :: OperandStack -- VM State
-  , o      :: Output -- VM State
-  , cp     :: ConstPool -- Program
-  , ia     :: InstructionAddress -- VM Program
-  , m      :: Memory -- VM Program
-  , gs     :: Globals -- 
-  , fns    :: FunctionList } -- Program
+  { ctx       :: Context -- VM State
+  , gvm       :: GlobalVarMap -- Program
+  , labels    :: SubprogramDir -- Program
+  , stack     :: OperandStack -- VM State
+  , out       :: Output -- VM State
+  , constpool :: ConstPool -- Program
+  , instaddr  :: InstructionAddress -- VM Program
+  , memory    :: Memory -- VM Program
+  , globals   :: Globals -- 
+  , fns       :: FunctionList } -- Program
